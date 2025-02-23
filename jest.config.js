a5051ec1a8d7d886c7a646182bd864cf8246b5b0
Vijayarahavan <vijayarahavan176@gmail.com>
2025-02-23 23:59:29 +0530

    feat: unit test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-reanimated|react-native-confetti-cannon|@react-native|@react-navigation)/)',
+  ],
 };
